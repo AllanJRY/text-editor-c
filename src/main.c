4f8ffab42d50e69c2d4fc5741b49e0b0f7d06b79
AllanJRY <jarry.allan@gmail.com>
2025-01-21 15:35:22 +0100

    feat: add raw mode

--- src/main.c
+++ src/main.c
@@ -1,7 +1,28 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios original_termios;
+
+void disable_raw_mode(void) {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
+void enable_raw_mode(void) {
+    tcgetattr(STDIN_FILENO, &original_termios);
+    atexit(disable_raw_mode);
+
+    struct termios raw = original_termios;
+    // ICANON ENABLE read byte by byte, no need to wait for EOF char.
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(void) {
-    printf("Hello text editor!");
+    enable_raw_mode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
 
