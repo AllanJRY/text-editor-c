abb94a387a5d02e4114a9ff517cef0cfcee3d116
AllanJRY <jarry.allan@gmail.com>
2025-01-31 10:09:04 +0100

    feat: add syxtax hl on search match

--- src/main.c
+++ src/main.c
@@ -41,7 +41,8 @@ typedef enum Editor_Key {
 
 typedef enum Editor_Highlight {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 } Editor_Highlight;
 
 /*** datas ***/
@@ -251,6 +252,7 @@ void editor_update_syntax(Editor_Row* row) {
 int editor_syntax_to_color(int hl) {
     switch (hl) {
         case HL_NUMBER: return 31;
+        case HL_MATCH:  return 34;
         default:        return 37;
     }
 }
@@ -503,6 +505,15 @@ void editor_find_callback(char* query, int key) {
     static int last_match_row = -1;
     static int direction      = 1;
 
+    static int saved_hl_line;
+    static char* saved_hl = NULL;
+
+    if (saved_hl) {
+        memcpy(editor_state.rows[saved_hl_line].hl, saved_hl, editor_state.rows[saved_hl_line].render_size);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+
     if (key == '\r' || key == '\x1b') {
         last_match_row = -1;
         direction  = 1;
@@ -530,6 +541,11 @@ void editor_find_callback(char* query, int key) {
             editor_state.cursor_y = curr_match_row;
             editor_state.cursor_x = editor_row_render_x_to_cursor_x(row, match - row->render);
             editor_state.row_offset = editor_state.rows_count;
+
+            saved_hl_line = curr_match_row;
+            saved_hl = malloc(row->render_size);
+            memcpy(saved_hl, row->hl, row->render_size);
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
